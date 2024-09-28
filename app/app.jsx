2e9c143f0392d68d93723513e6cd8f80daa6d263
RousDev <jhojanggarcial@gmail.com>
2024-09-28 12:25:32 -0500

    feat: add black style background.

--- app/app.jsx
+++ app/app.jsx
@@ -6,8 +6,7 @@ import { TooltipContent } from "./components/common/Content";
 
 export default function App() {
   return (
-    <main className="min-w-screen  flex dark:bg-black items-center justify-center min-h-screen">
-      <Header />
+    <main className="min-w-screen  flex  items-center justify-center min-h-screen">
       <TooltipContent />
     </main>
   );
