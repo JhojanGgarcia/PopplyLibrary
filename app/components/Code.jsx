eaa521b4d543d0dec42a5b526077e8dff0fa377d
RousDev <jhojanggarcial@gmail.com>
2024-09-28 12:49:31 -0500

    feat: add modules imports in shiki.

--- app/components/Code.jsx
+++ app/components/Code.jsx
@@ -1,20 +1,27 @@
-import { useEffect, useState } from 'react';
-import { codeToHtml, createHighlighter } from 'shiki';
+import { useEffect, useState } from "react";
 
 export default function ShikiCode({ code, lang, className, ...props }) {
-  const [html, setHtml] = useState('');
+  const [html, setHtml] = useState("");
 
-  
   useEffect(() => {
-    codeToHtml(code, { lang: 'css', theme: 'poimandres', full: true }).then((result) => {
-      const updatedHtml = result.replace(/background-color:[^;]+;/g, '');
+    async function loadShiki() {
+      const { codeToHtml } = await import("shiki"); // Importación dinámica
+
+      const result = await codeToHtml(code, {
+        lang: lang || "css",
+        theme: "poimandres",
+        full: true,
+      });
+      const updatedHtml = result.replace(/background-color:[^;]+;/g, "");
       setHtml(updatedHtml);
-    });
+    }
+
+    loadShiki();
   }, [code, lang]);
 
   return (
     <div
-      className={`text-sm   text-white ${className}`}
+      className={`text-sm text-white ${className}`}
       {...props}
       dangerouslySetInnerHTML={{ __html: html }}
     />
